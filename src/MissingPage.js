20a584177c3a80d425ed67fffa1d6fd787ebdf57
ArtistArcher <59206037+ArtistArcher@users.noreply.github.com>
2020-01-27 15:01:09 -0600

    Update MissingPage.js

--- src/MissingPage.js
+++ src/MissingPage.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// MissingPage.js
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 // Display missing page if user navigates to an invalid page
-
 const MissingPage = () => (
   <div>
-    <h1 className="not-found-title">
+    <h1 className="missing-title">
       The books you are looking for are not available.
     </h1>
     <div className="home-link">
       <Link to="/">Return to your book shelves</Link>
     </div>
   </div>
-);
+)
 
-export default MissingPage;
+export default MissingPage
