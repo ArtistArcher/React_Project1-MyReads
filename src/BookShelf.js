95435aa95d65a50e63bbba89aea6f7dc0334105e
ArtistArcher <59206037+ArtistArcher@users.noreply.github.com>
2020-01-30 15:36:59 -0600

    Update BookShelf.js

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,24 +1,27 @@
 // BookShelf.js
 import React, { Component } from 'react'
+// import Components
 import Book from './Book'
 
 // Define Book Shelf Component
 class BookShelf extends Component {
   render() {
     // Define Component Variables & Functions
-    const {shelf, books, onMove} = this.props
+    const {shelf, shelfName, books, onMove} = this.props
+    
     // Function: Find Books On This Shelf
-    const booksOnThisShelf = books.filter(book => book.shelf === shelf.key)
+    const booksOnThisShelf = books.filter(book => book.shelf === shelf)
     /*console.log("BookShelf: " + shelf.name)*/
     
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelf.name}</h2>
+        <h2 className="bookshelf-title">{shelfName}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
           {booksOnThisShelf.map(book => (
-            <Book key={book.id} book={book} shelf={shelf.key} onMove={onMove}/>
-          ))}
+           //book.shelf
+           <Book key={book.id} book={book} shelf={shelf} onMove={onMove}/>
+           ))}
           </ol>
         </div>
       </div>
