dff88cd6a2474a3b06bc80cac8824617ff95809f
ArtistArcher <59206037+ArtistArcher@users.noreply.github.com>
2020-01-30 15:39:44 -0600

    Update ShelfChanger.js

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -3,20 +3,24 @@ import React, { Component } from 'react'
 
 // Define ShelfChanger Component
 class ShelfChanger extends Component {
+  // Setup States for ShelfChanger
   state = {
     value: this.props.shelf,
   }
-  // Setting Initial Value to be changed in App.js
-  changeShelf = event => {
-    this.setState({value: event.target.value})
-    console.log("Change Shelf Value:" + event.target.value)
-    this.props.onMove(this.props.book, event.target.value)
-  }
 
   render() {
+    // Define Component Variables
+    const {book, onMove} = this.props
+
+    // Setting Value to be changed in App.js
+    const changeShelf = event => {
+      this.setState({value: event.target.value})
+      /*console.log("Change Shelf Value: " + event.target.value)*/
+      onMove(book, event.target.value)
+    }
     return (
       <div className="book-shelf-changer">
-        <select value={this.state.value} onChange={this.changeShelf}>
+        <select value={this.state.value} onChange={changeShelf}>
           <option value="move" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
